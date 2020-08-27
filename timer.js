e1482415980a3a62cdf8d2d6baf9b1c6ed93b199
Maria Perera <maria_perera@live.com>
2020-08-27 15:28:15 -0400

    added new icons, snowing tomatoes, attempted to add timer and popup, added media queries tocss

--- timer.js
+++ timer.js
@@ -1,8 +1,7 @@
 var seconds = 1500;
 console.log("Hey I am working");
 function getTimeInMinutes() {
-    if (document.getElementById("mins") != null)
-    document.getElementById("mins").innerHTML = 60;
+    document.getElementById("mins").innerHTML = seconds / 60;
     console.log("Mins working");
 }
 
